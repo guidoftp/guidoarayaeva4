3c74772c23d5044671249f48b5be9a34bf9f8c1f
Guido Andres Araya Gutierrez <guidoandres62@gmail.com>
2024-07-19 17:59:34 -0400

    Initial commit

---
+++ src/Firebase/Credenciales.tsx
@@ -0,0 +1,8 @@
+import const firebaseConfig = {
+    apiKey: "AIzaSyDpGLjri43uzq4YZxJYNsYvdOHG3rzfLG4",
+    authDomain: "eva4-fb2b4.firebaseapp.com",
+    projectId: "eva4-fb2b4",
+    storageBucket: "eva4-fb2b4.appspot.com",
+    messagingSenderId: "1009060093438",
+    appId: "1:1009060093438:web:00d9477c140a216b0834af"
+  };
